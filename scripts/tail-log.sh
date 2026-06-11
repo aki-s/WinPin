e0b785fcb170629a2fce2a264df4c91b6ef6b663
aki-s <sunny.day.dev@gmail.com>
2026-06-11 10:54:48 +0900

    chore: rename log tail script

--- scripts/tail-log.sh
+++ scripts/tail-log.sh
@@ -3,9 +3,11 @@ set -eu
 
 LOG_PATH="${HOME}/Library/Logs/WinPin/app.log"
 
-if [ -f "${LOG_PATH}" ]; then
-    cat "${LOG_PATH}"
-else
+if ! [ -f "${LOG_PATH}" ]; then
     echo "No WinPin log found at ${LOG_PATH}" >&2
-    exit 1
+    touch "${LOG_PATH}"
 fi
+
+wc "${LOG_PATH}"
+tail -f "${LOG_PATH}"
+
